3dc4746e06d116624a8b5e6b5b8f9750a5e3d5f8
hankim <donotask@for.email>
2020-02-26 12:40:42 +0900

    Implement array ADT

--- ds/array.c
+++ ds/array.c
@@ -1,17 +1,82 @@
 #include <unistd.h>
+#include <stdlib.h>
 
+# define ARRAY_SIZE 5
+
+int     f_strlen(char *str);
+int     *create(int n);
+int     retrieve(int *arr, int idx);
+void    store(int *arr, int i, int e);
+#include <stdio.h>
 int     main(void)
 {
-    int arr[5];
+
+    int arr[5] = {0};
+    int temp;
     int i;
 
     i = 0;
+    temp = -1;
+    /*
+    arr = create(5);
+    */
+    temp = retrieve(arr, 2) + 48;
+    write(1, &temp, 1);
     while (i < 5)
     {
-        arr[i] = i + 65;
-        write(1, &arr[i], 1);
+        store(arr, i, i + 1);
+        temp = retrieve(arr, i) + 48;
+        write(1, &temp, 1);
         i++;
     }
-    write(1, "\n", 1);
     return (0);
-} 
+}
+
+int     f_strlen(char *str)
+{
+    char *pt;
+
+    pt = str;
+    while (*str)
+    {
+        str++;
+    }
+    return (str - pt);
+}
+
+int     *create(int n)
+{
+    int *arr;
+    int i;
+
+    i = 0;
+    arr = (int *)malloc(sizeof(int) * n);
+    while (i < n)
+    {
+        arr[i] = 0;
+        i++;
+    }
+    return (arr);
+}
+
+int     retrieve(int *arr, int idx)
+{
+    if (idx >= ARRAY_SIZE || idx < 0)
+    {
+        write(1, "CANNOT RETRIEVE ARRAY ELEMENT!\n",
+         f_strlen("CANNOT RETRIEVE ARRAY ELEMENT!\n"));
+        return (-1);
+    }
+    return (arr[idx]);
+}
+
+void    store(int *arr, int i, int e)
+{
+    if (i >= ARRAY_SIZE || i < 0)
+    {
+        write(1, "ERROR\n", 6);
+        return ;
+    }
+    arr[i] = e;
+    return ;
+}
